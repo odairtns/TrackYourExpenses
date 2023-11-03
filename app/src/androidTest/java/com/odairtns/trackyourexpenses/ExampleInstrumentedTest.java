ffde7d8271a49ad12728ac75af46eb3cf550256d
Odair <odairtns@gmail.com>
2023-11-03 16:35:50 -0300

    Changed the DB name and updated gradle to track your expenses

--- app/src/androidTest/java/com/odairtns/trackyourexpenses/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/odairtns/trackyourexpenses/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.odairtns.trackyourtrip", appContext.getPackageName());
+        assertEquals("com.odairtns.trackyourexpenses", appContext.getPackageName());
     }
 }
